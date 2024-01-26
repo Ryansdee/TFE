af3efb7db320be7949bb858087ccb8906683cacd
Ryansdee <desch.ryann@gmail.com>
2024-01-26 13:17:55 +0100

    modifié :         ch.js 	modifié :         css/style.css 	modifié :         index.html

--- ch.js
+++ ch.js
@@ -12,18 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
       if (entry.isIntersecting) {
         chDiv.classList.add("fadeIn");
         observer.unobserve(chDiv);
-
-        // Ajoutez la classe "fadeIn" à l'image à l'intérieur de .ch
-        var imageInsideCh = chDiv.querySelector('img');
-        if (imageInsideCh) {
-          imageInsideCh.classList.add("fadeInn");
-        } else if (entry.intersectionRatio === 0 && entry.boundingClientRect.bottom < window.innerHeight) {
-          // Si l'élément est en bas de la page, masquez-le
-          chDiv.style.display = 'none';
-          imageInsideCh.style.display = 'none';
+      } else {
         chDiv.classList.remove("fadeIn");
-        imageInsideCh.classList.remove("fadeInn");
-        }
       }
     });
   }, options);
